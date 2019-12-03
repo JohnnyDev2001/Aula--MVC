3846095738ce4069a2a154618e9bab9d13e83f61
JohnnyDev2001 <johnny.devcode@gmail.com>
2019-12-03 17:40:15 -0300

    segurança

--- models/Users.php
+++ models/Users.php
@@ -109,8 +109,16 @@ class Users extends model {
     }
 
     public function atualizaInfoPerfil($id_user, $cep, $telefone, $endereco, $num_casa, $cidade, $estado, $referencia){
-        $sql = "UPDATE users_info SET cep = '$cep', telefone = '$telefone', endereco = '$endereco', num_casa = '$num_casa', cidade = '$cidade', estado = '$estado', referencia = '$referencia' where id_user='$id_user'";
+        $sql = "UPDATE users_info SET cep = :cep, telefone = :telefone, endereco = :endereco, num_casa = :num_casa, cidade = :cidade, estado = :estado, referencia = :referencia where id_user= :id_user";
         $sql = $this->db->prepare($sql);
+        $sql->bindValue(":cep", $cep);
+        $sql->bindValue(":telefone", $telefone);
+        $sql->bindValue(":endereco", $endereco);
+        $sql->bindValue(":num_casa", $num_casa);
+        $sql->bindValue(":cidade", $cidade);
+        $sql->bindValue(":estado", $estado);
+        $sql->bindValue(":referencia", $referencia);
+        $sql->bindValue(":id_user", $id_user);
         $sql->execute();
 
         return $sql;
