7197e29aa2432263b0f30ee26697f8daee647b48
JohnnyDev2001 <johnny.devcode@gmail.com>
2019-12-11 02:19:42 -0300

    js

--- assets/js/gambi.js
+++ assets/js/gambi.js
@@ -1,7 +1,7 @@
 var url_atual = window.location.href;
 var slide = document.querySelector('#carouselExampleControls');
 
-var bool = url_atual == 'http://localhost/MusicSpace/' || url_atual == 'http://localhost/MusicSpace/#';
+var bool = url_atual == 'http://localhost/MusicSpace/' || url_atual == 'http://localhost/MusicSpace/#' || url_atual == 'http://localhost/MusicSpace/#cont' || url_atual == 'http://localhost/MusicSpace/?s=&category=&filter%5Bbrand%5D%5B%5D=1&filter%5Bslider0%5D=0&filter%5Bslider1%5D=3779' || url_atual == 'http://localhost/MusicSpace/?s=&category=&filter%5Bbrand%5D%5B%5D=2&filter%5Bslider0%5D=0&filter%5Bslider1%5D=3779' || url_atual == 'http://localhost/MusicSpace/?s=&category=&filter%5Bbrand%5D%5B%5D=3&filter%5Bslider0%5D=0&filter%5Bslider1%5D=3779';
 
 console.log(bool);
 
